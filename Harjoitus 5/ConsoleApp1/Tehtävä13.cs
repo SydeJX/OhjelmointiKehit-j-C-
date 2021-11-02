a23698464c2716f6587473ac238b690790c32037
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:24 +0200

    Create Tehtävä13.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä13.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    public class Tehtävä13
+    {
+
+
+        public void tehtävä13()
+        {
+
+            string nimi = null;
+            Console.Write("Write your name to the console: ");
+            nimi = Console.ReadLine();
+            Console.Clear();
+            Console.Write(Reverse(nimi));
+            Console.ReadLine();
+        }
+
+
+        public string Reverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+
+            return new string(chars);
+        }
+        
+
+
+    }
+}
