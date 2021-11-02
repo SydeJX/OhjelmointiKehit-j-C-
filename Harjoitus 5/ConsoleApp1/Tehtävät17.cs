5e6ebc2f4bdbca5659c67f7d275423c2a03e70f6
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:38 +0200

    Create Tehtävät17.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävät17.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    public class Tehtävät17
+    {
+
+        public void tehtävät17()
+        {
+            const int luku = 3;
+            int change = 0;
+            int fer = 0;
+
+            for (int x = 0; x < 11; x++)
+            {
+                fer = luku * change;
+                change++;
+                Console.WriteLine(fer);
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
