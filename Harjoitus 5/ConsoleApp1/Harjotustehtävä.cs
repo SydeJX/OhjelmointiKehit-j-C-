f41ce9ada269c5f3b20e9d8573eeb57136591b89
SydeJX <sydeweiz@gmail.com>
2021-11-02 10:36:10 +0200

    Update Harjotustehtävä.cs

--- Harjoitus 5/ConsoleApp1/Harjotustehtävä.cs
+++ Harjoitus 5/ConsoleApp1/Harjotustehtävä.cs
@@ -12,7 +12,7 @@ namespace HarjousTehtävä
         {
             int num = 0;
             bool Start = true;
-            //meowoooooiuu
+            //meowoooooiuupåoåpoåpoåoåp
             Tehtävä1 t1 = new Tehtävä1();
             Tehtävä2 t2 = new Tehtävä2();
             Tehtävä5 t5 = new Tehtävä5();
