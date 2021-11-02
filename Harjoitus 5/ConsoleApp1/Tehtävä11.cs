3b3e9f163a89bf416d18c2defb3386353419b193
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:19 +0200

    Create Tehtävä11.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    class Tehtävä11
+    {
+
+
+
+        public void tehtävä11()
+        {
+
+            int age;
+            Console.Write("Write your age to the console: ");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Really? " + age + ", you look younger.");
+            Console.ReadLine();
+
+
+        }
+
+
+
+
+
+
+    }
+}
