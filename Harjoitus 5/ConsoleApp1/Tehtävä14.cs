3b40e0ce59a62801e2e54c43408fcc2cd15e0a24
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:26 +0200

    Create Tehtävä14.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä14.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    public class Tehtävä14
+    {
+
+
+        public void tehtävä14()
+        {
+
+            Console.Write("Write down two numbers: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            EvenorOdd(a,b);
+
+        }
+
+         void EvenorOdd(int input, int input2)
+        {
+            if (input <= 1 && input2 <= 1)
+            {
+                Console.WriteLine("Both are Negative");
+                Console.ReadLine();
+            } else if (input >= 1 && input2 >= 1) {
+                Console.WriteLine("Both are Postive");
+                Console.ReadLine();
+            }
+
+            if (input <= 1 && input2 >= 1)
+            {
+                Console.WriteLine("Another is Negative while the other is positive");
+                Console.ReadLine();
+            }
+            else if (input <= 1 && input2 >= 1)
+            {
+                Console.WriteLine("Another is Negative while the other is positive");
+                Console.ReadLine();
+            }
+
+           
+        }
+
+    }
+}
