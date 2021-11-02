f0199c19b14d7b75fb6c03b06a55f801a8f85b90
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:29 +0200

    Create Tehtävä15.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    public class Tehtävä15
+    {
+        
+        public void tehtävä15()
+        {
+            string word;
+            string[] themwords;
+            do
+            {
+                Console.Write("\nGive your scary sentence. Write 0 at the end: ");
+                word = Console.ReadLine();
+                themwords = word.Split(' ');
+                foreach (string x in themwords)
+                {
+                    Console.WriteLine(x);
+                }
+
+
+
+            } while (word != "0");
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
