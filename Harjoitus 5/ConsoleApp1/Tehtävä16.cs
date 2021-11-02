5a525d6c236ba7d93ef9ac39fe81d4bbb1b4c28a
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:31 +0200

    Create Tehtävä16.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä16.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    public class Tehtävä16
+    {
+        public void tehtävä16()
+        {
+
+            int forum = 0;
+            for (int i = 0; i < 19; i++)
+            {
+
+                if (forum < 11)
+                {
+                    forum++;
+                }
+                else
+                {
+                    forum = forum + 11;
+                }
+                Console.WriteLine(forum);
+
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
