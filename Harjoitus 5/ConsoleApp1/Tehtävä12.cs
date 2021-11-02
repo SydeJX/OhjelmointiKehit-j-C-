967a3e8d4240f793af9ba2ed31b4510d4006a68a
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:22 +0200

    Create Tehtävä12.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä12.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    class Tehtävä12
+    {
+
+
+        public void tehtävä12()
+        {
+
+            Console.WriteLine("Write down a number please: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = a;
+            a++;
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write(b + " ");
+            }
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+}
