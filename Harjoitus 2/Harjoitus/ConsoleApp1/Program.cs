998ad9b19db1e6c750d36bbea66e967245705549
SydeJX <sydeweiz@gmail.com>
2021-11-02 10:55:34 +0200

    69

--- Harjoitus 2/Harjoitus/ConsoleApp1/Program.cs
+++ Harjoitus 2/Harjoitus/ConsoleApp1/Program.cs
@@ -6,7 +6,48 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("KIrjoita luku: ");
+            int luku1;
+            luku1 = int.Parse(Console.ReadLine());
+            int luku2;
+            Console.Write("Kirjoita toinen luku: ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("x = " + luku1 + 3);
+            Console.ReadLine();
+
+            Console.Write("KIrjoita luku: ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Kirjoita toinen luku: ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("x = {0}", luku1 - 2);
+            Console.ReadLine();
+
+
+            Console.Write("KIrjoita luku: ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Kirjoita toinen luku: ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("x = {1}", luku1 - 2);
+            Console.ReadLine();
+
+            Console.Write("KIrjoita luku: ");
+            luku1 = int.Parse(Console.ReadLine());
+            Console.Write("Kirjoita toinen luku: ");
+            luku2 = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("x = {1}", luku1 - 2);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
         }
     }
 }
