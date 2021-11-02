abc15a9dd161eff1c349c51958da37bfa0ae45b2
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:17 +0200

    Create Tehtävä10.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    class Tehtävä10
+    {
+
+        public void tehtävä10()
+        {
+
+            int answernumber;
+            Console.Write("Please write the number you want to be calculated: ");
+            answernumber = int.Parse(Console.ReadLine());
+            for (int num = 1; num < 11; num++)
+            {
+                Console.Write(answernumber + " * " + num + " = ");
+                Console.Write(answernumber * num + "\n");
+            }
+            Console.Read();
+
+
+
+        }
+
+
+    }
+}
