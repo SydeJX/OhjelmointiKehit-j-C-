cf009a1b5900f701ba6cfb28b6d4d99f47bbf6ed
SydeJX <sydeweiz@gmail.com>
2021-11-02 09:57:36 +0200

    Create Tehtävä5.cs

---
+++ Harjoitus 5/ConsoleApp1/Tehtävä5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarjousTehtävä
+{
+    class Tehtävä5
+    {
+
+        public void tehtävä5()
+        {
+            string username = null;
+            Console.Write("What is your username: ");
+            username = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Welcome, " + username + " to your computer console.");
+            Console.ReadLine();
+        }
+
+    }
+}
