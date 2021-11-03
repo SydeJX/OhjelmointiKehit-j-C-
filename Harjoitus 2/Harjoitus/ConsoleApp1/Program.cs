1e473e6dedf0cfe5fb1c382afd2b0058eef3a6c7
SydeJX <sydeweiz@gmail.com>
2021-11-03 11:34:03 +0200

    dsads

--- Harjoitus 2/Harjoitus/ConsoleApp1/Program.cs
+++ Harjoitus 2/Harjoitus/ConsoleApp1/Program.cs
@@ -6,48 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("KIrjoita luku: ");
-            int luku1;
-            luku1 = int.Parse(Console.ReadLine());
-            int luku2;
-            Console.Write("Kirjoita toinen luku: ");
-            luku2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("x = " + luku1 + 3);
-            Console.ReadLine();
+            bool tt = true;
+            long a = 0;
 
-            Console.Write("KIrjoita luku: ");
-            luku1 = int.Parse(Console.ReadLine());
-            Console.Write("Kirjoita toinen luku: ");
-            luku2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("x = {0}", luku1 - 2);
-            Console.ReadLine();
-
-
-            Console.Write("KIrjoita luku: ");
-            luku1 = int.Parse(Console.ReadLine());
-            Console.Write("Kirjoita toinen luku: ");
-            luku2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("x = {1}", luku1 - 2);
-            Console.ReadLine();
-
-            Console.Write("KIrjoita luku: ");
-            luku1 = int.Parse(Console.ReadLine());
-            Console.Write("Kirjoita toinen luku: ");
-            luku2 = int.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine("x = {1}", luku1 - 2);
-            Console.ReadLine();
-
-
-
-
-
-
-
-
+            while (tt)
+            {
+                Random rnd = new Random();
+                a = rnd.Next(-1000000000, 999999999);
+                Console.WriteLine(a);
+                Console.ReadLine();
+                Console.Clear();
+            }
         }
     }
 }
