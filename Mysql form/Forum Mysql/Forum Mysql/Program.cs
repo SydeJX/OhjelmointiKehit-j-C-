a3bf2be1df680492e086e6c39f676432bd3b9e9d
SydeJX <sydeweiz@gmail.com>
2021-11-16 09:52:23 +0200

    Update Program.cs

--- Mysql form/Forum Mysql/Forum Mysql/Program.cs
+++ Mysql form/Forum Mysql/Forum Mysql/Program.cs
@@ -16,7 +16,7 @@ namespace Forum_Mysql
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new form1());
         }
     }
 }
