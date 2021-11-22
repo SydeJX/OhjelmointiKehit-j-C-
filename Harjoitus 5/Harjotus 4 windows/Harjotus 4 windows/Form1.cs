6cbf2a5b129822372e8d3e548710b78cf6f2846b
SydeJX <sydeweiz@gmail.com>
2021-11-22 08:39:26 +0200

    Some random works I did at school

---
+++ Harjoitus 5/Harjotus 4 windows/Harjotus 4 windows/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjotus_4_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var Time = DateTime.Now.ToString();
+            label7.Text = Time;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DateTime nyt = DateTime.Now;
+            DateTime birthday = dateTimePicker1.Value;
+            double yearsbeen = Math.Round((nyt - birthday).TotalDays);
+
+            yearsbeen = yearsbeen / 365.25;
+            label1.Text = yearsbeen + " Years";
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Harjoitus 5/Windows Harjotus/WIndows Harjotus/WIndows Harjotus/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WIndows_Harjotus
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 forum = new Form1();
+            Random rnd = new Random();
+            Point point1 = new Point();
+            int randomizer = 0;
+            randomizer = rnd.Next(-100, 900);
+            point1.X = randomizer;
+            randomizer = rnd.Next(-100, 900);
+            point1.X = randomizer;
+            this.Location = new Point(point1.X, Location.X);
+            this.Location = new Point(point1.Y, Location.X);
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
