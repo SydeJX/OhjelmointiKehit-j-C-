4cc129ca390bf47aa4a09bd9ee34dc89bead0d0a
SydeJX <sydeweiz@gmail.com>
2021-11-22 10:26:07 +0200

    fdsfds

--- Random tehtävät form/Random tehtävä form/Random tehtävä form/Form1.Designer.cs
+++ Random tehtävät form/Random tehtävä form/Random tehtävä form/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LukuText = new System.Windows.Forms.TextBox();
+            this.AnnaLabel = new System.Windows.Forms.Label();
+            this.AnswerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LukuText
+            // 
+            this.LukuText.Location = new System.Drawing.Point(175, 12);
+            this.LukuText.Name = "LukuText";
+            this.LukuText.Size = new System.Drawing.Size(145, 20);
+            this.LukuText.TabIndex = 0;
+            this.LukuText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.LukuText_TextChanged);
+            // 
+            // AnnaLabel
+            // 
+            this.AnnaLabel.AutoSize = true;
+            this.AnnaLabel.Location = new System.Drawing.Point(24, 15);
+            this.AnnaLabel.Name = "AnnaLabel";
+            this.AnnaLabel.Size = new System.Drawing.Size(125, 13);
+            this.AnnaLabel.TabIndex = 1;
+            this.AnnaLabel.Text = "Anna luku (-999 lopetus):";
+            // 
+            // AnswerLabel
+            // 
+            this.AnswerLabel.AutoSize = true;
+            this.AnswerLabel.Location = new System.Drawing.Point(43, 56);
+            this.AnswerLabel.Name = "AnswerLabel";
+            this.AnswerLabel.Size = new System.Drawing.Size(0, 13);
+            this.AnswerLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(375, 78);
+            this.Controls.Add(this.AnswerLabel);
+            this.Controls.Add(this.AnnaLabel);
+            this.Controls.Add(this.LukuText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox LukuText;
+        private System.Windows.Forms.Label AnnaLabel;
+        private System.Windows.Forms.Label AnswerLabel;
     }
 }
 
--- Random tehtävät form/Random tehtävä form/Random tehtävä form/Form1.cs
+++ Random tehtävät form/Random tehtävä form/Random tehtävä form/Form1.cs
@@ -12,9 +12,48 @@ namespace Random_tehtävä_form
 {
     public partial class Form1 : Form
     {
+
+        List<int> jono = new List<int>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LukuText_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (LukuText.Text == "-999")
+                {
+                    AnswerLabel.Text = "";
+                    int[] table = jono.ToArray();
+                    foreach(var vase in table)
+                    {
+                        AnswerLabel.Text += vase + " ";
+                    }
+                    AnswerLabel.Show();
+
+                }
+                else
+                {
+                    jono.Add(Int32.Parse(LukuText.Text));
+                    LukuText.Text = "";
+                }
+                if (e.KeyChar == (char)Keys.Escape)
+                {
+                    EmptyPaper();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AnswerLabel.Hide();
+            
+        }
+        private void EmptyPaper()
+        {
+            LukuText.Text = "";
         }
     }
 }
